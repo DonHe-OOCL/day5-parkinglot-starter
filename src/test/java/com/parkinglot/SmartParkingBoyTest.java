348637bca87fc7e80dab05b8c7f30337481d6460
HEDO <hedo@oocl.com>
2024-11-23 16:03:38 +0800

    test: supplement Story1-3 test case for SmartParkingBoy

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -1,8 +1,14 @@
 package com.parkinglot;
 
+import com.parkinglot.exception.NoAvailablePositionException;
+import com.parkinglot.exception.UnrecognizedParkingTicketException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SmartParkingBoyTest {
 
@@ -20,5 +26,195 @@ public class SmartParkingBoyTest {
         // Then
         assertEquals(19, secondParkingLot.getCurrentCapacity());
         assertEquals(secondParkingLot, ticket.getParkingLot());
+    }
+
+    @Test
+    public void should_return_ticket_when_park_given_a_car() {
+        // Given
+        SmartParkingBoy boy = new SmartParkingBoy();
+        ParkingLot parkingLot = new ParkingLot();
+        boy.workInParkingLot(parkingLot);
+        Car car = new Car();
+        // When
+        Ticket ticket = boy.park(car);
+        // Then
+        assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_return_car_when_fetch_given_a_ticket() {
+        // Given
+        SmartParkingBoy boy = new SmartParkingBoy();
+        ParkingLot parkingLot = new ParkingLot();
+        boy.workInParkingLot(parkingLot);
+        Car car = new Car();
+        // When
+        Ticket ticket = boy.park(car);
+        Car fetchCar = boy.fetch(ticket);
+
+        // Then
+        assertEquals(car, fetchCar);
+    }
+
+    @Test
+    public void should_print_error_message_when_fetch_given_wrong_ticket() {
+        // Given
+        SmartParkingBoy boy = new SmartParkingBoy();
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot otherParkingLot = new ParkingLot();
+        boy.workInParkingLot(parkingLot);
+        Car car = new Car();
+        Ticket ticket = new Ticket(otherParkingLot);
+        // When
+        UnrecognizedParkingTicketException exception =
+                assertThrows(UnrecognizedParkingTicketException.class, () -> boy.fetch(ticket));
+        // Then
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
+    }
+
+    @Test
+    public void should_print_error_message_when_fetch_given_used_ticket() {
+        // Given
+        SmartParkingBoy boy = new SmartParkingBoy();
+        ParkingLot parkingLot = new ParkingLot();
+        boy.workInParkingLot(parkingLot);
+        Car car = new Car();
+        // When
+        Ticket ticket = boy.park(car);
+        boy.fetch(ticket);
+        UnrecognizedParkingTicketException exception =
+                assertThrows(UnrecognizedParkingTicketException.class, () -> boy.fetch(ticket));
+        // Then
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
+    }
+
+    @Test
+    public void should_print_error_message_when_park_given_full_parkingLog_and_a_car() {
+        // Given
+        SmartParkingBoy boy = new SmartParkingBoy();
+        ParkingLot parkingLot = new ParkingLot();
+        boy.workInParkingLot(parkingLot);
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            cars.add(new Car());
+        }
+        Car car = new Car();
+
+        // When
+        try {
+            for (Car parkCar : cars) {
+                boy.park(parkCar);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        NoAvailablePositionException exception =
+                assertThrows(NoAvailablePositionException.class, () -> boy.park(car));
+
+        // Then
+        assertEquals("No available position.", exception.getMessage());
+    }
+
+    @Test
+    public void should_park_to_first_lot_when_park_given_two_parking_lot() {
+        // Given
+        SmartParkingBoy boy = new SmartParkingBoy();
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot seconedParkingLot = new ParkingLot();
+        boy.workInParkingLot(firstParkingLot);
+        boy.workInParkingLot(seconedParkingLot);
+        Car car = new Car();
+
+        // When
+        Ticket ticket = boy.park(car);
+
+        // Then
+        assertEquals(firstParkingLot, ticket.getParkingLot());
+    }
+
+    @Test
+    public void should_right_car_when_parkingBoy_park_different_lot_fetch_given_two_ticket() {
+        // Given
+        ParkingBoy boy = new ParkingBoy();
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        boy.workInParkingLot(firstParkingLot);
+        boy.workInParkingLot(secondParkingLot);
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+
+        // When
+        Ticket firstTicket = boy.park(firstCar);
+        Ticket secondTicket = boy.park(secondCar);
+        Car firstFetch = boy.fetch(firstTicket);
+        Car secondFetch = boy.fetch(secondTicket);
+
+        // Then
+        assertEquals(firstCar, firstFetch);
+        assertEquals(secondCar, secondFetch);
+    }
+
+    @Test
+    public void should_print_error_message_when_parkingBoy_work_two_lot_fetch_given_wrong_ticket() {
+        // Given
+        SmartParkingBoy boy = new SmartParkingBoy();
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        ParkingLot otherParkingLot = new ParkingLot();
+        boy.workInParkingLot(firstParkingLot);
+        boy.workInParkingLot(secondParkingLot);
+        Ticket ticket = new Ticket(otherParkingLot);
+        // When
+        UnrecognizedParkingTicketException exception =
+                assertThrows(UnrecognizedParkingTicketException.class, () -> boy.fetch(ticket));
+        // Then
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
+    }
+
+    @Test
+    public void should_print_error_message_when_parkingBoy_work_two_lot_fetch_given_used_ticket() {
+        // Given
+        SmartParkingBoy boy = new SmartParkingBoy();
+        Car car = new Car();
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        boy.workInParkingLot(firstParkingLot);
+        boy.workInParkingLot(secondParkingLot);
+        // When
+        Ticket ticket = boy.park(car);
+        Car ignore = boy.fetch(ticket);
+        UnrecognizedParkingTicketException exception =
+                assertThrows(UnrecognizedParkingTicketException.class, () -> boy.fetch(ticket));
+        // Then
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
+    }
+
+    @Test
+    public void should_print_error_message_when_parkingBoy_work_two_lot_park_given_full_parkingLog_and_a_car() {
+        // Given
+        ParkingBoy boy = new ParkingBoy();
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        boy.workInParkingLot(firstParkingLot);
+        boy.workInParkingLot(secondParkingLot);
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            cars.add(new Car());
+        }
+        Car car = new Car();
+
+        // When
+        try {
+            for (Car parkCar : cars) {
+                boy.park(parkCar);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        NoAvailablePositionException exception =
+                assertThrows(NoAvailablePositionException.class, () -> boy.park(car));
+
+        // Then
+        assertEquals("No available position.", exception.getMessage());
     }
 }
