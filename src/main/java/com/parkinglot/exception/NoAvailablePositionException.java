1ae3f285a2e1bf856c6a1032d9709bf3713d3294
HEDO <hedo@oocl.com>
2024-11-23 17:06:52 +0800

    refactor: reformat

--- src/main/java/com/parkinglot/exception/NoAvailablePositionException.java
+++ src/main/java/com/parkinglot/exception/NoAvailablePositionException.java
@@ -2,7 +2,7 @@ package com.parkinglot.exception;
 
 import static com.parkinglot.ParkingLot.NO_AVAILABLE_ERROR_MSG;
 
-public class NoAvailablePositionException extends RuntimeException{
+public class NoAvailablePositionException extends RuntimeException {
     public NoAvailablePositionException() {
         super(NO_AVAILABLE_ERROR_MSG);
     }
